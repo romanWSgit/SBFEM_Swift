9df35e2ade99bb410c7e242a9fe7e054a200e982
Roman Wallner- Silberhuber <roman_w_s@me.com>
2021-02-02 00:32:50 +0100

    started to implement math tests

---
+++ SBFEM/testMath.swift
@@ -0,0 +1,9 @@
+//
+//  testMath.swift
+//  SBFEM
+//
+//  Created by Roman Wallner- Silberhuber on 27.04.20.
+//  Copyright © 2020 Roman Wallner- Silberhuber. All rights reserved.
+//
+
+import Foundation
